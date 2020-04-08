d621d3bb7edea64ea57eaa3bcb31f072227849d2
kaka888970 <zoujiawei950@sina.com>
2020-04-08 14:13:13 +0800

    add eureka configuration

--- eureka8080/src/main/java/com/doublez/Eureka8080Application.java
+++ eureka8080/src/main/java/com/doublez/Eureka8080Application.java
@@ -2,7 +2,9 @@ package com.doublez;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer // 开启Eureka服务
 @SpringBootApplication
 public class Eureka8080Application {
 
