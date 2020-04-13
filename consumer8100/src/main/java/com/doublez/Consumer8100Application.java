5b14d8dc2078f37bf0e5770227eeaf3bd3d8db59
kaka888970 <zoujiawei950@sina.com>
2020-04-13 14:57:36 +0800

    add eureka configuration

---
+++ consumer8100/src/main/java/com/doublez/Consumer8100Application.java
@@ -0,0 +1,13 @@
+package com.doublez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Consumer8100Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Consumer8100Application.class, args);
+    }
+
+}
---
+++ consumer8100/src/test/java/com/doublez/Consumer8100ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doublez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Consumer8100ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
